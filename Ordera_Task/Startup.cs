b5a2f6cee79c7541289dc53e3564e833128a2319
eslamfcih <maldiny_eslam_2010@yahoo.com>
2019-02-11 22:20:03 +0200

    Add project files.

---
+++ Ordera_Task/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ordera_Task.Startup))]
+namespace Ordera_Task
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
